a2e2c065804a5164f878010baab35b851b422f1f
IrwinWangWang <60620547+IrwinWangWang@users.noreply.github.com>
2020-11-20 13:15:37 -0500

    Update coding_practice_2020_11_20.py

--- coding_practice_2020_11_20.py
+++ coding_practice_2020_11_20.py
@@ -9,4 +9,9 @@ per_pack = int(input ("how many cans are in a pack: "))
 cans = int(input("how many packs are there: "))
 print(f"there are {per_pack*cans} cans")
 
+Question 3:
 
+length = int(input ("what is the length of the prism: "))
+width = int(input("what is the width of the prism: "))
+height = int(input("what is the height of the prism: "))
+print(f"the volume is {length*width*height} units cubed")
