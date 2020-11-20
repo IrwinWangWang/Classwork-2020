d455894526e1e6e9ecfcaa4d5028012b826532d9
IrwinWangWang <60620547+IrwinWangWang@users.noreply.github.com>
2020-11-20 13:13:39 -0500

    Update coding_practice_2020_11_20.py

--- coding_practice_2020_11_20.py
+++ coding_practice_2020_11_20.py
@@ -1,2 +1,12 @@
+Question 1:
+  
 color = input ("what's your favourite colour: ")
 print (f"wow {color} that's my favourite color too")
+
+Question 2:
+
+per_pack = int(input ("how many cans are in a pack: "))
+cans = int(input("how many packs are there: "))
+print(f"there are {per_pack*cans} cans")
+
+
